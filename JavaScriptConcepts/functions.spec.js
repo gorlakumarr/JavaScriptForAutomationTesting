7c77c758c1cc8102687eabe0e08d2942b84b4f80
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-09 17:55:13 +0530

    Added Functions in JS

---
+++ JavaScriptConcepts/functions.spec.js
@@ -0,0 +1,26 @@
+//Block of code -> Function
+
+/*
+var - global level/function level
+
+let - global/block level - {}
+*/
+
+function add(a, b) {
+    return a + b
+}
+console.log(add(2, 3))
+
+/**
+ * Functions who dont have name called Anonymous functions
+ */
+
+// let sumOfInt = function (c, d) {
+//     return c + d
+// }
+// console.log(sumOfInt(2, 3))
+
+// or
+
+let sumOfInt = (c, d) => c + d // fat boy short operator
+console.log(sumOfInt(5, 6))

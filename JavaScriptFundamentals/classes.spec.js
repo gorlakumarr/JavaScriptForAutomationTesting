c7996d2c95d3a18f2eafc5f86e1ae38531a37a38
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-09 18:32:41 +0530

    Added Classes Code in JS

---
+++ JavaScriptFundamentals/classes.spec.js
@@ -0,0 +1,31 @@
+class Person {
+
+    //Class Var
+    age = 25
+    // location='Canada'
+
+    get location() {
+        return 'canada'
+    }
+}
+
+/**
+ * Constructor is method which executes by default when you create object of the class 
+ */
+
+constructor(firstName, lastName) //instance var
+{
+    this.firstName = firstName
+    this.lastName = lastName
+}
+
+//Method
+fullName()
+{
+    console.log(this.firstName + this.lastName)
+}
+
+let person = new Person("Tim", "Josh")
+console.log(person.age)
+console.log(person.location)
+console.log(person.fullName())
